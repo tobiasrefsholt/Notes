3da8e37bb6d0719d1651dbbe345cbdbfd25cff7a
tobias <tobias@refsholt.net>
2024-01-15 11:11:04 +0100

    Split frontend to its own repo

--- ApiEndpoints/Program.cs
+++ ApiEndpoints/Program.cs
@@ -3,6 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173");
+        });
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,8 +30,9 @@ builder.Services
 
 
 var app = builder.Build();
+app.UseCors();
 app.MapIdentityApi<AppUser>();
-app.UseHttpsRedirection();
+/*app.UseHttpsRedirection();*/
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
