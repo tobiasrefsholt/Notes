52f54920111178e9775a03f9257100ce6cb9c3ff
tobias <tobias@refsholt.net>
2024-01-16 23:19:25 +0100

    Single note view with markdown editor

--- router-app/src/Components/Dashboard/SingleNote.tsx
+++ router-app/src/Components/Dashboard/SingleNote.tsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import MDEditor from '@uiw/react-md-editor';
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import { useSelectedNote } from '../../pages/Dashboard';
 import Cookies from 'js-cookie';
-import { redirectDocument, useNavigate } from 'react-router-dom';
+import useFetchData from '../../hooks/useFetchData';
+
+type Note = {
+    guid: string;
+    content: string;
+    title: string;
+    tags: string[];
+    dateAdded: Date;
+}
 
 export default function SingleNote() {
-    return(
-        <div className='dashboard-sigle-note'>
+    const { selectedNote } = useSelectedNote();
+    const [value, setValue] = useState<string | undefined>("**Hello world!!!**");
+    const { data, isPending, error } = useFetchData("/GetNotes/" + selectedNote, [selectedNote]);
+    const {title, content}:Note = data;
 
-            <h1>This is a heading</h1>
-            <p>And this is a paragrath</p>
+    /* if (!selectedNote) return (
+        <h1>No selected note</h1>
+    ) */
+
+    return (
+        <div className='dashboard-sigle-note'>
+            <h1>{title}</h1>
+            <MDEditor value={value} onChange={setValue} />
         </div>
     );
 }
