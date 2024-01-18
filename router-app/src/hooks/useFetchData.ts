562ffc621bef583e393d9a436c75cca341a37820
tobias <tobias@refsholt.net>
2024-01-18 19:07:33 +0100

    Inplement useBearerToken hook

--- router-app/src/hooks/useFetchData.ts
+++ router-app/src/hooks/useFetchData.ts
@@ -11,33 +11,33 @@ export default function useFetchData<T>(apiPath: string, deps: React.DependencyL
     const [data, setData] = useState<T | null>(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState<string | null>(null);
-    
+
     useEffect(() => {
-        useBearerToken();
-        const token = localStorage.getItem("accessToken");
-        fetch("http://localhost:5214" + apiPath, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + token,
-            },
+        useBearerToken().then((token) => {
+            fetch("http://localhost:5214" + apiPath, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + token,
+                },
+            })
+                .then((res) => {
+                    console.log(res)
+                    if (!res.ok) {
+                        throw Error("Could not fetch resource");
+                    }
+                    return <T>res.json();
+                })
+                .then((data) => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
         })
-            .then((res) => {
-                console.log(res)
-                if(!res.ok) {
-                    throw Error("Could not fetch resource");
-                }
-                return <T>res.json();
-            })
-            .then((data) => {
-                setData(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            })
     }, deps)
 
     return { data, isPending, error }
