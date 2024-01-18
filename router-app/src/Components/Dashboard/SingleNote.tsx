08aad3e5f82b8c1242c6e3f8205a30794259f718
tobias <tobias@refsholt.net>
2024-01-18 14:18:42 +0100

    Change title to input field

--- router-app/src/Components/Dashboard/SingleNote.tsx
+++ router-app/src/Components/Dashboard/SingleNote.tsx
@@ -16,11 +16,15 @@ type Note = {
 export default function SingleNote() {
     const { guid } = useParams();
     const { data, isPending, error } = useFetchData<Note>("/GetNotes/" + guid, [guid]);
+
+    const [title, setTitle] = useState<string | undefined>("Untitled");
     const [value, setValue] = useState<string | undefined>("**Hello world!!!**");
 
     useEffect(() => {
+        setTitle(data?.title);
         setValue(data?.content);
-    }, [data?.content])
+    }, [data])
+
 
     return (
         <>
@@ -29,11 +33,13 @@ export default function SingleNote() {
             {
                 data &&
                 <main className='dashboard-sigle-note'>
-                    <h1>{data.title}</h1>
                     <div className="note-toolbar">
-                        <button>Save</button>
-                        <button>Rename</button>
-                        <button>Delete</button>
+                        <input className='note-heading' type='text' value={title} onChange={(e) => setTitle(e.target.value)} />
+                        <div className='toolbar-buttons'>
+                            <button>Save</button>
+                            <button>Rename</button>
+                            <button>Delete</button>
+                        </div>
                     </div>
                     <MDEditor value={value} onChange={setValue} visibleDragbar={false} />
                 </main>
