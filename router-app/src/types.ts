53ed0068a6253d72d8ac2b745ef3c312e3b73947
tobias <tobias@refsholt.net>
2024-01-21 15:20:10 +0100

    Added some global types

--- router-app/src/types.ts
+++ router-app/src/types.ts
@@ -1,5 +1,29 @@
 export type category = {
-    guid: string;
+    guid: string | null;
     parentGuid: string | null;
     name: string;
+}
+
+export type Note = {
+    guid: string;
+    title: string;
+    content: string;
+    categoryGuid: string | null;
+    categoryName: string | null;
+    dateAdded: Date;
+    lastChanged: Date;
+}
+
+export type InsertNote = {
+    guid?: string;
+    title?: string;
+    content?: string;
+    categoryGuid?: string | null;
+    categoryName?: string | null;
+}
+
+export type FetchResponse<T> = {
+    error: string | null;
+    isPending: boolean;
+    data: T;
 }
