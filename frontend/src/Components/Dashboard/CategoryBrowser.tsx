8cfc6619a857a062eb7765b0d5751de0d9127e5c
tobias <tobias@refsholt.net>
2024-01-26 14:25:51 +0100

    Removed unused function

--- frontend/src/Components/Dashboard/CategoryBrowser.tsx
+++ frontend/src/Components/Dashboard/CategoryBrowser.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import { useDashboardContext } from "../../pages/Dashboard";
 import { NoteCompact } from "../../types";
@@ -9,17 +8,12 @@ import NoteSidebarNewNote from "./NoteSidebarNewNote";
 export default function CategoryBrowser() {
     const { selectedCategory, categoriesFetch } = useDashboardContext();
     const { data, isPending, error, doFetch } = useFetch<NoteCompact[]>("/GetNotesByCategory", []);
-    const navigate = useNavigate();
 
     useEffect(() => {
         doFetch("GET", [selectedCategory?.guid || ""]);
     }, [selectedCategory])
 
     const headerText = selectedCategory !== null ? `${data?.length} Note(s) with category "${selectedCategory.name}"` : `${data?.length} Uncategorized note(s)`;
-
-    const handleEdit = () => {
-        navigate("/edit-category");
-    }
 
     return (
         <main className="category-browser">
