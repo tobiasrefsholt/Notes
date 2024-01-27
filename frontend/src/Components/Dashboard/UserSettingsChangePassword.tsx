92366cdc75fa7f0a920399fbd4cef59038c8d485
tobias <tobias@refsholt.net>
2024-01-27 23:46:50 +0100

    Fix change password

--- frontend/src/Components/Dashboard/UserSettingsChangePassword.tsx
+++ frontend/src/Components/Dashboard/UserSettingsChangePassword.tsx
@@ -2,17 +2,17 @@ import { useState } from "react";
 import useFetch from "../../hooks/useFetch"
 
 export default function UserSettingsChangePassword() {
-    const [currentPassword, setCurrentPassword] = useState("");
+    const [oldPassword, setOldPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
-    const changePasswordFetch = useFetch("/changePassword", []);
+    const changePasswordFetch = useFetch("/manage/info", []);
 
     const handleChangePassword = () => {
         const requestBody = {
-            currentPassword,
+            oldPassword,
             newPassword
         }
         changePasswordFetch.doFetch("POST", [], requestBody);
-        setCurrentPassword("");
+        setOldPassword("");
         setNewPassword("");
     }
 
@@ -21,9 +21,9 @@ export default function UserSettingsChangePassword() {
             <h2>Change password</h2>
             <div className="card-content">
                 <label htmlFor="current-password">Current password</label>
-                <input type="password" id="current-password" />
+                <input type="password" id="current-password" value={oldPassword} onChange={(e) => setOldPassword(e.target.value)} />
                 <label htmlFor="new-password">New password</label>
-                <input type="password" id="new-password" />
+                <input type="password" id="new-password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
             </div>
             <div>
                 <button onClick={handleChangePassword}>Submit</button>
