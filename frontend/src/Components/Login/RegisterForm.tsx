f72b77ab9511b270c5a82291ff00901294a12ec7
tobias <tobias@refsholt.net>
2024-01-30 14:05:33 +0100

    Error handling on registration form

--- frontend/src/Components/Login/RegisterForm.tsx
+++ frontend/src/Components/Login/RegisterForm.tsx
@@ -5,11 +5,28 @@ type RegisterFormProps = {
     setShowLogin: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
+type RegisterResponse = true | {
+    type: string;
+    title: string;
+    status: 400;
+    errors: {
+        InvalidEmail?: string[];
+        DuplicateEmail?: string[];
+        DuplicateUserName?: string[];
+        PasswordTooShort?: string[];
+        PasswordRequiresNonAlphanumeric?: string[];
+        PasswordRequiresDigit?: string[];
+        PasswordRequiresLower?: string[];
+        PasswordRequiresUpper?: string[];
+        PasswordRequiresUniqueChars?: string[];
+    }
+}
+
 export default function RegisterForm({ setShowLogin }: RegisterFormProps) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isRegistered, setIsRegistered] = useState(false);
-    const { error, isPending, data, doFetch } = useFetch<boolean | null>("/register", [], "Registration failed");
+    const { error, isPending, data, doFetch } = useFetch<RegisterResponse | null>("/register", [], "Registration failed");
 
     const handleRegister = () => {
         doFetch("POST", [], { email, password }, false);
@@ -21,6 +38,43 @@ export default function RegisterForm({ setShowLogin }: RegisterFormProps) {
         setIsRegistered(true);
     }, [data])
 
+    function EmailError() {
+        if (data === true || data === null)
+            return "";
+
+        if (data?.errors.InvalidEmail?.length)
+            return (<p>{data.errors.InvalidEmail[0]}</p>)
+
+        if (data?.errors.DuplicateEmail?.length)
+            return (<p>{data.errors.DuplicateEmail[0]}</p>)
+
+        if (data?.errors.DuplicateUserName?.length)
+            return (<p>{data.errors.DuplicateUserName[0]}</p>)
+    }
+
+    function PasswordError() {
+        if (data === true || data === null)
+            return "";
+
+        if (data.errors.PasswordTooShort?.length)
+            return (<p>{data.errors.PasswordTooShort[0]}</p>);
+
+        if (data.errors.PasswordRequiresNonAlphanumeric?.length)
+            return (<p>{data.errors.PasswordRequiresNonAlphanumeric[0]}</p>);
+
+        if (data.errors.PasswordRequiresDigit?.length)
+            return (<p>{data.errors.PasswordRequiresDigit[0]}</p>);
+
+        if (data.errors.PasswordRequiresLower)
+            return (<p>{data.errors.PasswordRequiresLower[0]}</p>);
+
+        if (data.errors.PasswordRequiresUpper)
+            return (<p>{data.errors.PasswordRequiresUpper[0]}</p>);
+
+        if (data.errors.PasswordRequiresUniqueChars)
+            return (<p>{data.errors.PasswordRequiresUniqueChars[0]}</p>);
+    }
+
     return (
         <>
             <h2 className="card-header">Register new account</h2>
@@ -31,17 +85,19 @@ export default function RegisterForm({ setShowLogin }: RegisterFormProps) {
                     <div>
                         <label>Email:</label>
                         <input type="email" value={email} onChange={e => setEmail(e.target.value)} autoFocus={true} />
+                        <EmailError />
                     </div>
                     <div>
                         <label>Password:</label>
                         <input type="password" value={password} onChange={e => setPassword(e.target.value)} autoComplete="new-password" />
+                        <PasswordError />
                     </div>
                     <button onClick={handleRegister}>Register</button>
                 </>
             }
             {
                 isPending &&
-                <p>Logging in...</p>
+                <p>Creating account...</p>
             }
             {
                 error &&
