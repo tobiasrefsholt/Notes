0363261a070e16e5168db330c83531f9a48416d0
tobias <tobias@refsholt.net>
2024-02-01 14:53:40 +0100

    Better error handling when fetching bearer tokens

--- frontend/src/hooks/useBearerToken.ts
+++ frontend/src/hooks/useBearerToken.ts
@@ -15,39 +15,46 @@ type loginFailedResponse = {
 export default async function useBearerToken(): Promise<string | null> {
     const accessTokenExpires = +<string>localStorage.getItem("accessTokenExpires") || 0;
     const currentTimestamp = new Date().getTime();
+    const refreshToken = localStorage.getItem("refreshToken");
 
     // Abort refresh if token has more than 30 min until expiration
-    if (accessTokenExpires - 10 > currentTimestamp) {
+    if (accessTokenExpires > currentTimestamp) {
         return localStorage.getItem('accessToken');
     };
 
-    const refreshToken = localStorage.getItem("refreshToken");
+    try {
+        // Throw error if refresh token is missing
+        if (!refreshToken)
+            throw Error();
 
-    if (refreshToken === null) return null;
+        // Fetch new access token
+        const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/refresh", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ refreshToken: refreshToken })
+        });
 
-    const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/refresh", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ refreshToken: refreshToken })
-    });
+        // Throw error if response is not ok
+        if (!response.ok)
+            throw Error();
 
-    if (!response.ok) {
-        return null;
-    }
+        const data: loginSuccessfulResponse | loginFailedResponse = await response.json();
 
-    const data: loginSuccessfulResponse | loginFailedResponse = await response.json();
+        // Trow error if token is missing from response
+        if (!("tokenType" in data))
+            throw Error();
 
-    if ("tokenType" in data) {
+        // If successful, set localstorage and return token.
         localStorage.setItem('accessToken', data.accessToken);
         localStorage.setItem('accessTokenExpires', (currentTimestamp + data.expiresIn * 1000).toString());
         localStorage.setItem('refreshToken', data.refreshToken);
         return data.accessToken;
+    } catch (error) {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('accessTokenExpires');
+        localStorage.removeItem('refreshToken');
+        return null;
     }
-
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('accessTokenExpires');
-    localStorage.removeItem('refreshToken');
-    return null;
 }
