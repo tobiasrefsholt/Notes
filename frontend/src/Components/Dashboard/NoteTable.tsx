e7f66c8497e182117a91b75fe729520cbdac807b
tobias <tobias@refsholt.net>
2024-02-06 13:05:36 +0100

    Display dates better

--- frontend/src/Components/Dashboard/NoteTable.tsx
+++ frontend/src/Components/Dashboard/NoteTable.tsx
@@ -5,7 +5,7 @@ type PostsTableProps = {
     data: NoteCompact[];
 }
 
-export default function NoteTable({data}:PostsTableProps) {
+export default function NoteTable({ data }: PostsTableProps) {
     const navigate = useNavigate();
     return (
         <table>
@@ -25,14 +25,62 @@ export default function NoteTable({data}:PostsTableProps) {
                             {note.categoryName}
                         </td>
                         <td>
-                            {note.lastChanged}
+                            <DisplayTimeSinceDate dateStr={note.lastChanged} />
                         </td>
                         <td>
-                            {note.dateAdded}
+                            <DisplayIsoDate dateStr={note.dateAdded} />
                         </td>
                     </tr>
                 ))}
             </tbody>
         </table>
     )
+}
+
+type DateProps = {
+    dateStr: string;
+}
+
+function DisplayTimeSinceDate({ dateStr }: DateProps) {
+    const currentDate = new Date();
+    const date = convertUTCDateToLocalDate(new Date(dateStr));
+    const difference = currentDate.getTime() - date.getTime();
+
+    // Less than 1 minute
+    if (difference < 60 * 1000) {
+        return"Less than minute ago";
+    }
+
+    // Less than 1 hour
+    if (difference < 60 * 60 * 1000) {
+        return Math.floor(difference / 1000 / 60) + " minutes ago";
+    }
+
+    // Less than 24 hours
+    if (difference < 24 * 60 * 60 * 1000) {
+        return Math.floor(difference / 1000 / 60 / 60) + " hours ago";
+    }
+
+    // Less than 1 week
+    if (difference < 7 * 24 * 60 * 60 * 1000) {
+        return Math.floor(difference / 1000 / 60 / 60 / 24) + " days ago";
+    }
+
+    return date.toDateString();
+}
+
+function convertUTCDateToLocalDate(date: Date) {
+    var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
+
+    var offset = date.getTimezoneOffset() / 60;
+    var hours = date.getHours();
+
+    newDate.setHours(hours - offset);
+
+    return newDate;
+}
+
+function DisplayIsoDate({dateStr}: DateProps) {
+    const date = new Date(dateStr);
+    return date.toISOString().substring(0, 10);
 }
